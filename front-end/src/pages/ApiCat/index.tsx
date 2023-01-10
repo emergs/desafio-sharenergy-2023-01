4e2926ba3db8d513fa15b365fef00c368a4f6621
emerson gonçalves <emer_gs@hotmail.com>
2023-01-10 11:58:25 -0300

    fix:alinhamento dos elementos da pagina apicat

--- front-end/src/pages/ApiCat/index.tsx
+++ front-end/src/pages/ApiCat/index.tsx
@@ -1,4 +1,4 @@
-import { Button, TextField } from "@mui/material"
+import { Box, Button, Container, TextField } from "@mui/material"
 import { useState } from "react"
 import Body from "../../components/Body";
 import { apiCat } from "../../services/api";
@@ -25,15 +25,19 @@ const ApiCat = () => {
 
   return (
     <Body>
-      <TextField
-        id="outlined-basic"
-        label="Digite o status-code"
-        variant="outlined"
-        value={statusCode}
-        onChange={(e) => setStatusCode(e.target.value)}
-      />
-      <Button variant="contained" onClick={(event) => getDataCat(event)}>Enviar</Button>
-      <img src={''} alt="status code cat api" />
+      <Container sx={{ display: 'flex', justifyContent: 'center', marginTop: '10%' }}>
+        <Box sx={{ width: '400px', display: 'flex', flexDirection: 'column', gap: 2 }}>
+          <img src={''} alt="status code cat api" />
+          <TextField
+            id="outlined-basic"
+            label="Digite o status-code"
+            variant="outlined"
+            value={statusCode}
+            onChange={(e) => setStatusCode(e.target.value)}
+          />
+          <Button variant="contained" onClick={(event) => getDataCat(event)}>Enviar</Button>
+        </Box>
+      </Container>
     </Body>
 
   )
